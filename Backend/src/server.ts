a594c9e74a453bf60e49923d7a2446cf9055bd46
Pa1mekala37 <pavanmekala64@gmail.com>
2024-02-25 02:22:23 +0530

    url update

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://mern-url-shortener-1.onrender.com"],
+    origin: ["http://localhost:3000", "https://mern-url-shortener-1-k8qr.onrender.com"],
     credentials: true,
   })
 );
