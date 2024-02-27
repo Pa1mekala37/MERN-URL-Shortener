faf0f1e3bc64db1d822eae022d2d465f774d84eb
Pa1mekala37 <pavanmekala64@gmail.com>
2024-02-27 19:20:53 +0530

    responsiveness issue in the input field resolved

--- Frontend/src/components/FormContainer/FormContainer.tsx
+++ Frontend/src/components/FormContainer/FormContainer.tsx
@@ -25,7 +25,7 @@ const FormContainer: React.FunctionComponent<IFormContainerProps> = (props) => {
   return (
     <div className="container mx-auto p-2">
       <div className="bg-banner my-8 rounded-xl bg-cover bg-center">
-        <div className="w-full h-full rounded-xl p-20 backdrop-brightness-50">
+        <div className="w-full h-full rounded-xl p-5 sm:p-10 md:p-10 lg:p-10 xl:p-20 backdrop-brightness-50">
           <h2 className="text-white text-4xl text-center pb-4">URL Shortner</h2>
           <p className="text-white text-center pb-2 text-xl font-extralight">
           Unleash the Power of Concise Connections with TinyLinker - Shorten, Share, Swiftly Connect!
@@ -33,14 +33,14 @@ const FormContainer: React.FunctionComponent<IFormContainerProps> = (props) => {
           <form onSubmit={handleSubmit}>
             <div className="flex">
               <div className="relative w-full">
-                <div className="absolute inset-y-0 start-0 flex items-center ps-2 pointer-events-none text-slate-800">
+                {/* <div className="absolute inset-y-0 start-0 flex items-center ps-2 pointer-events-none text-slate-800">
                 TinyLinker /
-                </div>
+                </div> */}
                 <input
                   type="text"
                   placeholder="add your link"
                   required
-                  className="block w-full p-4 ps-32 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500"
+                  className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500"
                   value={fullUrl}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                     setFullUrl(e.target.value)
