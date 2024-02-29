9d378b2786d908441f853e5b0c199128af0f1d8e
PAVAN MEKALA <104497104+Pa1mekala37@users.noreply.github.com>
2024-03-01 00:33:25 +0530

    Update Constants.ts

--- Frontend/src/helpers/Constants.ts
+++ Frontend/src/helpers/Constants.ts
@@ -1,4 +1,6 @@
-export const serverUrl =
+export const serverUrl: string =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:5001/api'
-    : 'https://mern-url-shortener-1.onrender.com/api';
+    : import.meta.env.MODE === 'production'
+      ? 'https://mern-url-shortener-1.onrender.com/api'
+      : 'https://fragile-beret-fox.cyclic.app/api';
