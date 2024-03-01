e623c59f3637db29f29fce6eeeb18cae4926f01e
PAVAN MEKALA <104497104+Pa1mekala37@users.noreply.github.com>
2024-03-01 09:48:38 +0530

    CORS updated

--- Frontend/src/helpers/Constants.ts
+++ Frontend/src/helpers/Constants.ts
@@ -1,6 +1,1 @@
-export const serverUrl: string =
-  import.meta.env.MODE === 'development'
-    ? 'http://localhost:5001/api'
-    : import.meta.env.MODE === 'production'
-      ? 'https://mern-url-shortener-production.up.railway.app/api'
-      : '';
+export const serverUrl: string = import.meta.env.MODE === 'development'? 'http://localhost:5001/api': import.meta.env.MODE === 'production'? 'https://mern-url-shortener-production.up.railway.app/api': '';
